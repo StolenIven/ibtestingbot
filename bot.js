05091ca01eac21078aa280285991b3aa460760e5
StolenIven <36015719+StolenIven@users.noreply.github.com>
2019-01-07 17:09:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,26 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+bot.on("ready", async () => {
+
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(`testbot`, {type: "WATCHING"});
+
+});
+
+bot.on("guildMemberAdd", function(member) {
+    member.guild.channels.find("name", "logs").sendMessage(member.toString() + " has joined the server.");
+});
+
+bot.on("guildMemberRemove", function(member) {
+  member.guild.channels.find("name", "logs").sendMessage(member.toString() + " has left the server.");
+});
+
+bot.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
